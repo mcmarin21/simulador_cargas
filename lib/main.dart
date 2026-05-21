fd6e8e4aa0e37dbd9c0e4dcba34effc01e6c4e90
mcmarin21 <candresm2200@alumno.ipn.mx>
2026-05-20 23:23:37 -0600

    [FIX] Implementacion de Material Design 3

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.lightBlue),
+        useMaterial3: true,
       ),
       home: AppMain(),
     );
@@ -21,14 +22,11 @@ class MyApp extends StatelessWidget {
 }
 
 class AppMain extends StatelessWidget{
-
-  const AppMain({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface),
-      child: Container(
+    return Scaffold(
+      body: Container(
 
       ),
     );
