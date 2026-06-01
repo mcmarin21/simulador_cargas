9fc6484d7fd4d137062adbe105fe109d471cd920
mcmarin21 <candresm2200@alumno.ipn.mx>
2026-06-01 17:35:22 -0600

    Divisior horizontal temporal

--- lib/ui/components/divisor_horizontal.dart
+++ lib/ui/components/divisor_horizontal.dart
@@ -2,11 +2,37 @@ import 'package:flutter/material.dart';
 
 class DivisorHorizontal extends StatelessWidget{
 
+  final double width;
+  final GestureDragUpdateCallback onDragUpdate;
+
+  const DivisorHorizontal({
+    required this.width,
+    required this.onDragUpdate,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return(
-      Container()
+    final color = Theme.of(context).colorScheme.outline;
+
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onHorizontalDragUpdate: onDragUpdate,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.resizeColumn,
+        child: SizedBox(
+          width: width,
+          child: Center(
+            child: Container(
+              width: 4,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(2),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
