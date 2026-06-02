703d15feac4eaa19c4faa778f382ce66eca29ff2
mcmarin21 <candresm2200@alumno.ipn.mx>
2026-06-01 18:57:51 -0600

    Metodo para obtener distancia entre dos posiciones diferentes

--- lib/domain/posicion.dart
+++ lib/domain/posicion.dart
@@ -1,6 +1,13 @@
+import 'dart:math';
+
 class Posicion {
   double posX = 0;
   double posY = 0;
 
   Posicion(this.posX, this.posY);
+
+  double obtenerDistancia(Posicion pos1, Posicion pos2){
+    return sqrt(pow(pos1.posX - pos2.posX, 2) + pow(pos1.posY - pos2.posY, 2));
+  }
+  
 }
