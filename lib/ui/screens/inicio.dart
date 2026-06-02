0c0a101586c2c8d18335badfdd0e5aa957669da1
Abi <aldamapedrozoabigail@gmail.com>
2026-06-01 22:20:42 -0600

    texto

--- lib/ui/screens/inicio.dart
+++ lib/ui/screens/inicio.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:simulador_cargas/core/app_colors.dart';
 
-
-Widget responsiveBox({
-  required Widget child,
-  double widthFactor = 1.0,  // Por defecto toma el 100% del ancho
-  double heightFactor = 1.0, // Por defecto toma el 100% del alto
-  Alignment alignment = Alignment.center, // Por defecto se alinea al centro
-}) {
-  return FractionallySizedBox(
-    widthFactor: widthFactor,
-    heightFactor: heightFactor,
-    alignment: alignment,
-    child: child,
-  );
-}
 
 class Inicio extends StatefulWidget {
   const Inicio({super.key});
@@ -27,24 +14,33 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
 
-
-  final double screenHeight = MediaQuery.of(context).size.height;
-  final double screenwidth = MediaQuery.of(context).size.width;
-
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenwidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: AppColors.fondo,
-        body: Stack(
-          children: [   
-           Positioned(
-                top: screenHeight * 0.5,
-                right:screenwidth * 0.5,
-                child: 
-                Image.asset("assets/images/Inicio.gif", height: screenwidth * 0.3,
-                width: screenwidth * 0.3)
-              ),
-          ],
-        ),
+      // 1. El Scaffold ya se encarga del color de fondo en toda la pantalla
+      backgroundColor: AppColors.fondo, 
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: screenHeight - (screenwidth * 0.75),
+            right: screenwidth * 0.4, 
+            child: Image.asset(
+              "assets/images/Inicio.gif",
+              width: screenwidth * 0.8,
+              height: screenwidth * 0.8,
+            ),
+          ),
+      Positioned(
+        bottom: screenHeight - (screenwidth * 0.2),
+            right: screenwidth * 0.4,
+        child: Text("Simulador de cargas",style:GoogleFonts.dynaPuff(
+          fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,),)
+        )
+        ],
+      ),
     );
   }
 }
