81fe7e78b31381adb4bba5ce033e58d5ac0beed9
mcmarin21 <candresm2200@alumno.ipn.mx>
2026-06-02 14:17:13 -0600

    Refactor colorTematico a colorIcono

--- lib/ui/components/carga_display.dart
+++ lib/ui/components/carga_display.dart
@@ -10,21 +10,20 @@ class CargaDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 1. Identificamos los datos visuales dinámicos según el signo matemático de la magnitud
+
     IconData icono = Icons.circle_outlined;
-    Color colorTematico = Colors.grey;
-    String tipoTexto = "Neutra";
+    Color colorIcono = Colors.grey;
 
     if (carga.magnitud > 0) {
       icono = Icons.add_circle_outline_rounded;
-      colorTematico = Colors.redAccent;
+      colorIcono = Colors.redAccent;
     } else if (carga.magnitud < 0) {
       icono = Icons.remove_circle_outline_rounded;
-      colorTematico = Colors.blueAccent;
+      colorIcono = Colors.blueAccent;
     }
     else if(carga.magnitud == 0){
       icono = Icons.circle_outlined;
-      colorTematico = Colors.grey;
+      colorIcono = Colors.grey;
     }
 
     return Card(
@@ -36,30 +35,26 @@ class CargaDisplay extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // El ícono representativo de física con su color temático
-            Icon(icono, color: colorTematico, size: 32),
+
+            Icon(icono, color: colorIcono, size: 32),
             const SizedBox(width: 12),
 
-            // Columna informativa con la info estructurada de la carga
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  // Renglón superior: Nombre + Chip de Estado dinámico
                   Text(
                     carga.nombre,
                     style: TextTheme.of(context).titleMedium,
                   ),
 
-                  // Renglón medio: Valor con notación científica estilizada (Ej: 5.0 × 10^-6 C)
                   Text(
                     "Valor: ${carga.magnitud} × 10^${carga.prefijo} C",
                     style: TextTheme.of(context).bodyMedium,
                   ),
                   const SizedBox(height: 2),
 
-                  // Renglón inferior: Coordenadas físicas reales y tipo de carga
                   Text(
                     "Pos: (${carga.pos.x.toStringAsFixed(0)}, ${carga.pos.y.toStringAsFixed(0)})",
                     style: TextTheme.of(context).bodySmall,
